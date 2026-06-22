8a06d4ca9f21064fe5705fa1f3968b94d40b8a37
prashanth <munja.prashanth09@gmail.com>
2026-06-22 19:23:42 +0530

    Your update message

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import CoreServices from "./components/CoreServices";
+import Stats from "./components/Stats";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
       <Hero />
-    </div>
+      <CoreServices />
+      <Stats />
+      <Footer />
+    </>
   );
 }
 
